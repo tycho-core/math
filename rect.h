b5cf3a4c66a82207d12d642a8fc127c317b02ef8
Martin Slater <mslater@darkstorm.com.au>
2020-02-24 23:08:30 +1100

    add extra accessors

--- rect.h
+++ rect.h
@@ -5,7 +5,7 @@
 //////////////////////////////////////////////////////////////////////////////
 #if _MSC_VER > 1000
 #pragma once
-#endif  // _MSC_VER
+#endif // _MSC_VER
 
 #ifndef __RECT_H_E57B8F15_31FF_4F8B_9B1E_F4AEE2654075_
 #define __RECT_H_E57B8F15_31FF_4F8B_9B1E_F4AEE2654075_
@@ -25,62 +25,65 @@ namespace tycho
 namespace math
 {
 
-	/// 2d rectangle
-	template<class T>
+    /// 2d rectangle
+    template <class T>
     struct rect
     {
     public:
-		typedef rect<T> this_type;
-		
+        typedef rect<T> this_type;
+
     public:
-		/// constructor
-		rect(T x, T y, T width, T height);
-		
-		/// default constructor, zero initialises
-		rect();
-		
-		/// \returns the width of the rectangle
-		T get_width() const;
-		
-		/// \returns the height of the rectangle
-		T get_height() const;
-		
-		T get_minx() const
-			{ return m_x; }
-			
-		T get_miny() const
-			{ return m_y; }
+        /// constructor
+        rect(T x, T y, T width, T height);
 
-		T get_maxx() const
-			{ return m_x + m_width; }
+        /// constructor
+        rect(const vector2<T>& pos, const vector2<T>& size);
 
-		T get_maxy() const
-			{ return m_y + m_height; }
-		
-		/// \returns the top left corner of the rectangle
-		vector2<T> get_top_left() const;
-		
-		/// \returns the top right corner of the rectangle
-		vector2<T> get_top_right() const;
-		
-		/// \returns the bottom left corner of the rectangle
-		vector2<T> get_bottom_left() const;
-		
-		/// \returns the top right corner of the rectangle
-		vector2<T> get_bottom_right() const;		
-		
+        /// default constructor, zero initialises
+        rect();
+
+        T x() const { return m_x; }
+        T y() const { return m_y; }
+        T width() const { return m_width; }
+        T height() const { return m_height; }
+
+        /// \returns the width of the rectangle
+        T get_width() const;
+
+        /// \returns the height of the rectangle
+        T get_height() const;
+
+        T get_minx() const { return m_x; }
+
+        T get_miny() const { return m_y; }
+
+        T get_maxx() const { return m_x + m_width; }
+
+        T get_maxy() const { return m_y + m_height; }
+
+        /// \returns the top left corner of the rectangle
+        vector2<T> get_top_left() const;
+
+        /// \returns the top right corner of the rectangle
+        vector2<T> get_top_right() const;
+
+        /// \returns the bottom left corner of the rectangle
+        vector2<T> get_bottom_left() const;
+
+        /// \returns the top right corner of the rectangle
+        vector2<T> get_bottom_right() const;
+
     private:
-		T m_x, m_y;
-		T m_width, m_height;
+        T m_x, m_y;
+        T m_width, m_height;
     };
-    
-#include "rect.inl"    
-    typedef rect<float> rectf;
-    typedef rect<double> rectd;
-    typedef rect<int> recti;
-    
 
-} // end namespace
-} // end namespace
+#include "rect.inl"
+    typedef rect<float>  rectf;
+    typedef rect<double> rectd;
+    typedef rect<int>    recti;
+
+} // namespace math
+} // namespace tycho
 
 #endif // __RECT_H_E57B8F15_31FF_4F8B_9B1E_F4AEE2654075_
--- rect.inl
+++ rect.inl
@@ -5,62 +5,73 @@
 //////////////////////////////////////////////////////////////////////////////
 
 /// constructor
-template<class T>
-rect<T>::rect(T x, T y, T width, T height) :
-	m_x(x),
-	m_y(y),
-	m_width(width),
-	m_height(height)
+template <class T>
+rect<T>::rect(T x, T y, T width, T height)
+    : m_x(x)
+    , m_y(y)
+    , m_width(width)
+    , m_height(height)
 
-{}
+{
+}
+
+template <class T>
+rect<T>::rect(const vector2<T>& pos, const vector2<T>& size)
+    : m_x(pos.x())
+    , m_y(pos.y())
+    , m_width(size.x())
+    , m_height(size.y())
+{
+}
 
 /// default constructor, zero initialises
-template<class T>
-rect<T>::rect() :
-	m_x(0),
-	m_y(0),
-	m_width(0),
-	m_height(0)
-{}
+template <class T>
+rect<T>::rect()
+    : m_x(0)
+    , m_y(0)
+    , m_width(0)
+    , m_height(0)
+{
+}
 
 /// \returns the width of the rectangle
-template<class T>
+template <class T>
 T rect<T>::get_width() const
 {
-	return m_width;
+    return m_width;
 }
 
 /// \returns the height of the rectangle
-template<class T>
+template <class T>
 T rect<T>::get_height() const
 {
-	return m_height;
+    return m_height;
 }
 
 /// \returns the top left corner of the rectangle
-template<class T>
+template <class T>
 vector2<T> rect<T>::get_top_left() const
 {
-	return vector2<T>(m_x, m_y);
+    return vector2<T>(m_x, m_y);
 }
 
 /// \returns the top right corner of the rectangle
-template<class T>
+template <class T>
 vector2<T> rect<T>::get_top_right() const
 {
-	return vector2<T>(m_x + m_width, m_y);
+    return vector2<T>(m_x + m_width, m_y);
 }
 
 /// \returns the bottom left corner of the rectangle
-template<class T>
+template <class T>
 vector2<T> rect<T>::get_bottom_left() const
 {
-	return vector2<T>(m_x, m_y + m_height);
+    return vector2<T>(m_x, m_y + m_height);
 }
 
 /// \returns the top right corner of the rectangle
-template<class T>
+template <class T>
 vector2<T> rect<T>::get_bottom_right() const
 {
-	return vector2<T>(m_x + m_width, m_y + m_height);
+    return vector2<T>(m_x + m_width, m_y + m_height);
 }
